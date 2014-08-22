54225a0d722309615af1476a14a620d035eaa7c4
LeuhFahn <leuh.fahn@gmail.com>
2014-08-22 22:49:58 +0200

    update config project

--- Assets/Code/CGameCreator.cs
+++ Assets/Code/CGameCreator.cs
@@ -4,11 +4,11 @@ using System.Collections;
 public class CGameCreator : MonoBehaviour {
 
 	public GameObject m_prefabGame;
-
 	// Use this for initialization
 	void Start () 
 	{
-		
+		GameObject game = ((GameObject) GameObject.Instantiate(m_prefabGame));
+		game.name = "_Game";
 	}
 	
 	// Update is called once per frame
