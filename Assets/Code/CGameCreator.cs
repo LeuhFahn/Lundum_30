388cb8971191c48cdc8e59926ff558f426cc66a4
LeuhFahn <leuh.fahn@gmail.com>
2014-08-22 23:49:04 +0200

    set GameCreator as singleton

--- Assets/Code/CGameCreator.cs
+++ Assets/Code/CGameCreator.cs
@@ -3,17 +3,23 @@ using System.Collections;
 
 public class CGameCreator : MonoBehaviour {
 
+	static int m_instanceCount = 0;
 	public GameObject m_prefabGame;
-	// Use this for initialization
-	void Start () 
-	{
-		GameObject game = ((GameObject) GameObject.Instantiate(m_prefabGame));
-		game.name = "_Game";
-	}
 	
-	// Update is called once per frame
+	void Awake () 
+	{
+		if(m_instanceCount++ == 0){
+			GameObject game = ((GameObject) GameObject.Instantiate(m_prefabGame));
+			game.name = "_Game";
+		}
+	}
+
 	void Update () 
 	{
 	
+	}
+	
+	void OnDestroy() {
+		m_instanceCount--;
 	}
 }
