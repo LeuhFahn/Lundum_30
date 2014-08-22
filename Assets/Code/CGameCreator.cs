0dd6bbc6896121edccc7bb139c6a121b0e6c91e9
LeuhFahn <leuh.fahn@gmail.com>
2014-08-22 21:16:23 +0200

    modif mmm.cpp

---
+++ Assets/Code/CGameCreator.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CGameCreator : MonoBehaviour {
+
+	public GameObject m_prefabGame;
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+}
---
+++ Assets/Code/mmm.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+
+int main()
+{
+	std::cout << "MMM suce des Queues" << std::endl;
+	return 0;
+}
