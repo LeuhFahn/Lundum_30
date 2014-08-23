fa6c6917401f2965ccd104658df74b0d9361afdd
NicolasNin <nicolas.ninin@gmail.com>
2014-08-23 18:44:25 +0200

    enlever foutreninin et remplacer par maj

--- Assets/Code/CRoad.cs
+++ Assets/Code/CRoad.cs
@@ -73,6 +73,6 @@ public class CRoad : MonoBehaviour {
 	{
 		m_bConstructionIsOver = true;
 		m_MeshRoad.renderer.material.color = Color.blue;
-		CConstantes.Game.FoutreNinin();
+		CConstantes.Game.MaJ(m_PlanetOrigin,m_PlanetDestination);
 	}
 }
