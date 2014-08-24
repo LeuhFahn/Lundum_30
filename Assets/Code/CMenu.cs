be08b6c08fa6bd3e0c7a9b6d3347008f8878e164
mmickyman <mmickyman@gmail.com>
2014-08-24 02:21:57 +0200

    MMMenu principal
    Scene du menu principal, prefab de bouton de base

---
+++ Assets/Code/CMenu.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CMenu : MonoBehaviour {
+
+	public GameObject goMenuPrincipal;
+	public GameObject goCredits;
+
+	void Start()
+	{
+		OpenMenuPrincipal();
+	 }
+
+	public void LoadNextLevel ()
+	{
+		Debug.Log("Lancer scene");
+//		if(Application.loadedLevel < Application.levelCount)
+//		{
+//			Application.LoadLevel(Application.loadedLevel+1);
+//		}
+	}
+	
+	public void OpenMenuPrincipal()
+	{
+		goMenuPrincipal.SetActive(true);
+		goCredits.SetActive(false);
+	}
+
+	public void OpenCredits()
+	{
+		goMenuPrincipal.SetActive(false);
+		goCredits.SetActive(true);
+	}
+
+}
